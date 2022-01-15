792fc49913834f1d14867c32d5115d4ab0cd8de2
siva-raam <75804677+siva-raam@users.noreply.github.com>
2022-01-15 15:44:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,13 @@
 provider "aws" {
-  region = var.aws_region
+  region = "ap-northeast-1"
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
 
 resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
+  ami           = "ami-032d6db78f84e8bf5" 
+  key_name = "jenkins"
+  instance_type = "t2.micro"
   tags= {
-    Name = var.tag_name
+    Name = "My-EC2"
   }
 }
